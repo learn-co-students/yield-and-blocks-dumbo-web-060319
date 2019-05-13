c63bb9d7cca33aabd9af104daf00d868d352960b
Alexander Schelchere <ascellie@gmail.com>
2019-05-12 21:09:30 -0400

    OKAY time for GoT

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
+def hello_t(items)
+
+    if block_given?
+        i = 0
+        while i < items.length
+            yield(items[i])
+            i += 1
+        end
+
+        items
+    else
+        puts "Hey! No block was given!"
+    end
 
 end
 
 # call your method here!
 
+hello_t(["Tim", "Tom", "Jim"])
